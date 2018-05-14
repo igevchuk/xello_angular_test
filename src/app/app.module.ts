82143def55d21d86716a05cf7791a265cb7953f8
Julianna Gevchuk <igevchuk@reol.com>
2018-05-14 17:17:51 -0400

    Add angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { TooltipComponent } from './tooltip/tooltip.component';
+import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { TooltipComponent } from './common/tooltip/tooltip.component';
+import { AppRouter } from './app.routing';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
     TooltipComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouter
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/common/tooltip/tooltip.component.ts
+++ src/app/common/tooltip/tooltip.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tooltip',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tooltip.component.css']
 })
 export class TooltipComponent implements OnInit {
+  @Input() show: Boolean;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.show);
+
   }
 
 }
